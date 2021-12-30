f951620ca5683f815e7666d0a6fe21c0e363a8e7
park-jaeha <jaeha1477@naver.com>
2021-12-30 16:49:54 +0900

    add mssql

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -29,16 +29,39 @@ const LoginScreen = (props)=>{
     
         return () => backHandler.remove();
     }, []);
+    const [userID, setUserID] = React.useState('JH');
+    const [userPW, setUserPW] = React.useState('test');
 
+    const nextInput = React.useRef();
 
     const LoginFunc = ()=>{
-        props.LoginFunc();
+        props.LoginFunc(userID, userPW);
     }
 
     return(
         <ImageBackground source = {fish} style ={{flex:1,}} blurRadius={10}>
             <View style ={styles.LoginScreenWrap} >
+                <View style = {{flex:1.5, justifyContent:'center'}}>
                 <Text style ={{alignSelf:'center', width:200, textAlign:'center', color :'#EAB0FC', backgroundColor:'#000'}}>Test Ledger page</Text>
+                </View>
+                <View style = {{flex:1 ,alignItems:'center'}}> 
+                <TextInput
+                    style={styles.LoginScreenTextInput}
+                    placeholder='ID'
+                    value={userID}
+                    onChangeText={setUserID}
+                    onEndEditing={ () => {nextInput.current.focus();} }
+                />
+                <TextInput
+                    style={styles.LoginScreenTextInput}
+                    placeholder='PW'
+                    value={userPW}
+                    onChangeText={setUserPW}
+                    ref={nextInput}
+                    secureTextEntry={true}
+                />
+                </View>
+                <View style ={{flex:1}}>
                 <TouchableOpacity
                     style={{
                     width: 120 ,
@@ -52,6 +75,7 @@ const LoginScreen = (props)=>{
                 >
                     <Text style ={{ color:'#fff', textAlign:'center'}}>메인페이지</Text>
                 </TouchableOpacity>
+                </View>
             </View>
         </ImageBackground>
     );
@@ -61,7 +85,18 @@ const styles = StyleSheet.create({
     LoginScreenWrap :{
         flex:1,
         justifyContent:'center',
-    }
+    },
+    LoginScreenTextInput: {
+        width: '80%',
+        height: 30,
+        borderColor: '#cccccc',
+        borderBottomWidth: 2,
+        borderRadius: 15,
+        marginLeft:7,
+        marginBottom: 20,
+        padding:5,
+        textAlign:'left',
+    },
 });
 
 export default LoginScreen;
